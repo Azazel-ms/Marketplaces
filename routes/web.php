ea2bb24751f233e6e1914a17004bb3d609750f18
Alejandro Leon <85299747+AlejandroLeon-Mc@users.noreply.github.com>
2021-06-07 09:50:47 -0500

    20200607-Alejandro
    Creación de controlador y nuevo grupo de rutas, cambio al dashboard para llamar al controlador.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SourceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Route::get('/sourceItems', [SourceController::class, 'itemSources'])->name('Source_items');
+
+Route::group(['prefix' => 'source'], function(){ 
+
+	Route::get(
+    	'items',
+    	[SourceController::class, 'itemSources']
+	)->name('Source_items');	
+
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
